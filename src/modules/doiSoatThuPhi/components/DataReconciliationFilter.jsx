0430621365fa070585211f962382ba8c8c42c47e
thangnv2 <nvthang2k.nd@gmail.com>
2026-02-06 23:38:40 +0700

    feat: Responsive đối soát thu phí

--- src/modules/doiSoatThuPhi/components/DataReconciliationFilter.jsx
+++ src/modules/doiSoatThuPhi/components/DataReconciliationFilter.jsx
@@ -14,80 +14,68 @@ function DataReconciliationFilter({ initialValues, onSearch }) {
 
   return (
     <FilterSection>
-      <Form
-        form={form}
-        layout="inline"
-        onFinish={onFinish}
-        initialValues={initialValues}
-        style={{ display: "block" }}
-      >
+      <Form form={form} layout="inline" onFinish={onFinish} initialValues={initialValues} style={{ display: "block" }}>
         <FilterRow>
-          <Col span={3}>
-            <Form.Item name="tuyen" >
+          <Col xl={3} md={6} sm={12} xs={12}>
+            <Form.Item name="tuyen">
               <CSelect placeholder="Tuyến" allowClear>
                 <SelectOption value="nb-lc">Nội bài - Lào Cai</SelectOption>
               </CSelect>
             </Form.Item>
           </Col>
-          <Col span={3}>
-            <Form.Item name="tram" >
+          <Col xl={3} md={6} sm={12} xs={12}>
+            <Form.Item name="tram">
               <CSelect placeholder="Chọn trạm" allowClear>
                 <SelectOption value="km6">Km6</SelectOption>
                 <SelectOption value="ic13">IC13</SelectOption>
               </CSelect>
             </Form.Item>
           </Col>
-          <Col span={3}>
-            <Form.Item name="maEtag" >
+          <Col xl={3} md={6} sm={12} xs={12}>
+            <Form.Item name="maEtag">
               <CSelect placeholder="Mã Etag" allowClear />
             </Form.Item>
           </Col>
-          <Col span={3}>
-            <Form.Item name="maGDBE" >
+          <Col xl={3} md={6} sm={12} xs={12}>
+            <Form.Item name="maGDBE">
               <CSelect placeholder="Mã GD BE" allowClear />
             </Form.Item>
           </Col>
-          <Col span={3}>
-            <Form.Item name="bienSoXe" >
+          <Col xl={3} md={6} sm={12} xs={12}>
+            <Form.Item name="bienSoXe">
               <CSelect placeholder="Biển số xe" allowClear />
             </Form.Item>
           </Col>
-          <Col span={3}>
-            <Form.Item name="loaiGD" >
+          <Col xl={3} md={6} sm={12} xs={12}>
+            <Form.Item name="loaiGD">
               <CSelect placeholder="Loại GD" allowClear />
             </Form.Item>
           </Col>
-          <Col span={6}>
-            <Form.Item name="loaiGDLech" >
-              <CSelect
-                mode="multiple"
-                placeholder="Loại giao dịch lệch"
-
-                options={LOAI_GD_LECH_OPTIONS}
-                maxTagCount="responsive"
-              />
+          <Col xl={6} md={12} sm={24} xs={24}>
+            <Form.Item name="loaiGDLech">
+              <CSelect mode="multiple" placeholder="Loại giao dịch lệch" options={LOAI_GD_LECH_OPTIONS} maxTagCount="responsive" />
             </Form.Item>
           </Col>
-          <Col span={3}>
-            <Form.Item name="loaiSaiLech" >
+          <Col xl={3} md={6} sm={12} xs={12}>
+            <Form.Item name="loaiSaiLech">
               <CSelect placeholder="Loại sai lệch" allowClear />
             </Form.Item>
           </Col>
 
-          <Col span={3}>
-            <Form.Item name="trangThaiGiaiTrinh" >
+          <Col xl={3} md={6} sm={12} xs={12}>
+            <Form.Item name="trangThaiGiaiTrinh">
               <CSelect placeholder="Trạng thái giải trình" allowClear />
             </Form.Item>
           </Col>
 
-          <Col span={6}>
-            <Form.Item name="date" >
-              <CRangePicker format="DD/MM/YYYY" style={{ width: '100%' }} />
+          <Col xl={6} md={12} sm={24} xs={24}>
+            <Form.Item name="date">
+              <CRangePicker format="DD/MM/YYYY" style={{ width: "100%" }} />
             </Form.Item>
           </Col>
-          <Col span={3}>
+          <Col xl={3} md={6} sm={12} xs={12}>
             <Form.Item>
-              <CButton type="primary" htmlType="submit" style={{ width: '100%' }}>
+              <CButton type="primary" htmlType="submit" style={{ width: "100%" }}>
                 Tìm kiếm
               </CButton>
             </Form.Item>
